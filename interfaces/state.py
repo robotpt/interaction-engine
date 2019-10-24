727414275637b12e4bb74b57d38b0af0bfb39195
Audrow Nash <audrowna@usc.edu>
2019-10-23 20:10:38 -0700

    Remove user input from state

--- interfaces/state.py
+++ interfaces/state.py
@@ -5,9 +5,6 @@ import datetime
 state_db = PickledDatabase()
 
 state_db.create_key_if_not_exists(
-    'user_input'
-)
-state_db.create_key_if_not_exists(
     'user_name',
     tests=lambda x: type(x) is str
 )
