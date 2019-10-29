8fc0f45323755ab569e03c421ab6cf39f8c48454
Audrow Nash <audrowna@usc.edu>
2019-10-29 13:04:33 -0700

    Make params and state databases into functions with file path

--- data_structures/params.py
+++ data_structures/params.py
@@ -1,8 +1,12 @@
 from pickled_database import PickledDatabase
 
-params_file = 'params.pkl'
-params_db = PickledDatabase(params_file)
 
-params_db.create_key_if_not_exists(
-    'param1', 1
-)
+def get_params_database(file='params.pkl'):
+
+    params_db = PickledDatabase(file)
+
+    params_db.create_key_if_not_exists(
+        'param1', 1
+    )
+
+    return params_db
--- data_structures/state.py
+++ data_structures/state.py
@@ -2,14 +2,17 @@ from pickled_database import PickledDatabase
 import datetime
 
 
-state_file = 'state.pkl'
-state_db = PickledDatabase(state_file)
+def get_state_database(file='state.pkl'):
 
-state_db.create_key_if_not_exists(
-    'user_name',
-    tests=lambda x: type(x) is str
-)
-state_db.create_key_if_not_exists(
-    'first_meeting',
-    tests=lambda x: type(x) is datetime.datetime
-)
+    state_db = PickledDatabase(file)
+
+    state_db.create_key_if_not_exists(
+        'user_name',
+        tests=lambda x: type(x) is str
+    )
+    state_db.create_key_if_not_exists(
+        'first_meeting',
+        tests=lambda x: type(x) is datetime.datetime
+    )
+
+    return state_db
